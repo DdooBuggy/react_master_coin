4ae5d33aeaff1d0c95d58fcb2b5b55db66bbbbbe
DdooBuggy <yhiyo03@gmail.com>
2022-05-11 15:39:03 +0900

    github deploy

--- src/Router.tsx
+++ src/Router.tsx
@@ -10,7 +10,7 @@ interface IRouterProps {
 
 function Router({}: IRouterProps) {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Coins />} />
         <Route path="/:coinId" element={<Coin />}>
